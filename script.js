8759913f0f3b55b5f329fad19ba04a0dcd201d02
LiecardoAntonio <liecardoantonio24@gmail.com>
2024-10-17 15:30:03 +0700

    progress3

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ const deleteBtn = document.getElementById('delete');
 const multiplyBtn = document.getElementById('multiply');
 const divideBtn = document.getElementById('divide');
 const addBtn = document.getElementById('add');
-const substractBtn = document.getElementById('subtract');
+const subtractBtn = document.getElementById('subtract');
 const equalBtn = document.getElementById('equal');
+const operationsBtn = [multiplyBtn, divideBtn, addBtn, subtractBtn]
 
 let currInputs = []; //store all the input including the operations
 let leftParenthesisCount = 0;
@@ -38,12 +39,66 @@ for(const input of btnInputs) {
   })
 }
 
-//operations button clicked
+//check if we can input operation to the currInput
+const allowOperationInput = (operationInput) => {
+  // first condition check if the operation is the first input, if yes than only '-' is allowed
+  if(currInputs.length==0 || currInputs[0]=='-') {
+    if(operationInput === '+' || operationInput ==='×' || operationInput === '÷') {
+      return false;
+    } else return true;
+  } else {
+    //condition when there is already a value inside the currInputs
+    console.log('masuk');
+    let currInputValue = currInputs[currInputs.length-1];
+    console.log(currInputValue);
+    console.log(currInputs.length-1);
+    if(currInputValue === '+' || currInputValue === '-' || currInputValue === '×' || currInputValue === '÷') {
+      //if the previous value is also a operation, replace the operation by removing the last index
+      currInputs.pop();
+      inputOrResult.innerHTML = currInputs.join('');
+      return true;
+    } else {
+      //if the previous input is not operation, then simpy add it to the currInputs
+      return true
+    }
+  }
+}
+
+//operation button clicked
+for(const operationBtn of operationsBtn) {
+  operationBtn.addEventListener('click', () => {
+    const allowance = allowOperationInput(operationBtn.innerText);
+
+    if(allowance) {
+      inputOrResult.innerHTML += operationBtn.innerText; //replace the view
+      currInputs.push(operationBtn.innerText); //add the button to input storage
+      console.log(currInputs);
+    } else {
+      console.log(currInputs);
+      return;
+    }
+  })
+}
+
+//functional button clicked
 clearBtn.addEventListener('click', () => {
   //clear the view and all the current inputs
   inputOrResult.innerHTML = '';
   currInputs.splice(0, currInputs.length); //delete all inputs
   console.log(currInputs);
 })
+
+deleteBtn.addEventListener('click', () => {
+  currInputs.pop();
+  console.log(currInputs);
+  inputOrResult.innerHTML = currInputs.join('');
+})
+
+equalBtn.addEventListener('click', () => {
+
+})
+
+
+
 
 
